5fe56a346776210e26c4ba62c2b417e6f5f79efa
beersteiner <johndouglasstein@gmail.com>
2017-12-01 13:40:20 -0500

    Realized I was calculating the same features over and over again unnecessarily, so I made the requisite modifications to make it efficient.

--- iceberg/ice.r
+++ iceberg/ice.r
@@ -33,6 +33,17 @@ records <- dbGetQuery(mydb, 'SELECT idn FROM train')[,1]#[1:100]
 rlen <- length(records)
 
 
+## Calculate features of training set
+message('\nCalculating Features for Trainign Set')
+X <- data.frame()
+pb <- txtProgressBar(style=3)
+for(r in records) {
+    X <- rbind(X, getFeat('train', r)[,features])
+    setTxtProgressBar(pb, r/rlen)
+}
+close(pb)
+
+
 ## V-Fold Cross Validation
 if(TRUE) {
     V <- 10
@@ -40,7 +51,7 @@ if(TRUE) {
     LogLoss = data.frame(train=NA, test=NA)
     ## Loop through each fold
     for(v in 1:V) {
-        message(p('Training on fold ', v))
+        message(p('\nTraining on fold ', v))
         r.first <- ((v - 1) * fsize) + 1 ## calc. starting index for test
         if(v == V) { ## calc. stopping index for test
             r.last <- rlen ## if last fold
@@ -50,43 +61,30 @@ if(TRUE) {
         records.train <- records[-c(r.first:r.last)] ## training set
         records.test <- records[r.first:r.last]      ## testing set
         ## Build training features
-        Xr <- data.frame()
-        pb <- txtProgressBar(style=3); prog=0
-        for(r in records.train) {
-            Xr <- rbind(Xr, getFeat('train', r)[,features])
-            prog=prog+1
-            setTxtProgressBar(pb, prog/length(records.train))
-        }
-        ## Fit to a Logistic Regression Model
+        Xr <- X[records.train,]
         model <- glm(formula = is_iceberg ~ .,
                      data = Xr,
                      family = binomial(logit) )
         ## Estimate probabilities & error for training data
         pr.train <- predict(model, Xr, type='response');
         LogLoss[v,'train'] <- logloss(Xr$is_iceberg, pr.train)
-        ## Error[v,'train'] <- sum(xor(Xr$is_iceberg, pr.train>0.5)) / length(pr.train)
-        ## Error[v,'train.base'] <- (function(x) min(x, 1-x)) (sum(Xr$is_iceberg) / nrow(Xr))
         ## Estimate probabilities for test data
-        Xs <- data.frame(); for(r in records.test) Xs <- rbind(Xs, getFeat('train', r)[,features])
+        Xs <- X[records.test,]
         pr.test <- predict(model, Xs, type='response')
         LogLoss[v,'test'] <- logloss(Xs$is_iceberg, pr.test)
-        ## Error[v,'test'] <- sum(xor(Xs$is_iceberg, pr.test>0.5)) / length(pr.test)
-        ## Error[v,'test.base'] <- (function(x) min(x, 1-x)) (sum(Xs$is_iceberg) / nrow(Xs))
-        print(LogLoss[v,])
+        ##print(LogLoss[v,])
     }
+    message('\nLog Loss per fold:')
+    print(LogLoss)
+    message('\nAverage Log Loss')
     print(colMeans(LogLoss))
 }
 
 
 ## Train Model on entire training set
 message('\nTraining Final Model')
-Xrt <- data.frame()
-pb <- txtProgressBar(style=3)
-for(r in records) {
-    setTxtProgressBar(pb, r/rlen)
-    Xrt <- rbind(Xrt, getFeat('train', r)[,features])
-}
-model <- glm(formula = is_iceberg ~ ., data = Xrt, family = binomial(logit))
+model <- glm(formula = is_iceberg ~ ., data = X, family = binomial(logit))
+
 
 ## Classify Unseen Data
 message('\nClassifying Unseen Test Data')
